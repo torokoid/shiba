60a97f2dcd930a5138410d78354231b2ccff374f
torokoid <39209977+torokoid@users.noreply.github.com>
2019-06-14 20:50:28 +0900

    Add files via upload

---
+++ my.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg932\cocoartf1671\cocoasubrtf500
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+jQuery(document).ready(function () \{\
+  jQuery.goup(\{\
+    locationOffset: 50,\
+    bottomOffset: 20,\
+    hideUnderWidth: 1025\
+  \});\
+  jQuery('a').smoothScroll();\
+\});}
